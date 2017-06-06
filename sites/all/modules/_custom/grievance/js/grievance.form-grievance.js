11e76b6caf853222222967f920c6bb5f4ad691d5
sam@clevernamehere.com <root@localhost.localdomain>
2017-06-06 10:50:55 +0000

    Make status required; add a report showing grievances without statuses

---
+++ sites/all/modules/_custom/grievance/js/grievance.form-grievance.js
@@ -0,0 +1,28 @@
+(function($) {
+	$(document).ready(function () {
+
+		if (Drupal.jsAC) {
+		  Drupal.jsAC.prototype.select = function (node) {
+		    this.input.value = $(node).data('autocompleteValue');
+		    // Custom: add an event trigger
+		    $(this.input).trigger('autocompleteSelect', $(node).data('autocompleteValue'));
+		  };
+		}
+
+
+		//alert('Hello, world.');
+		console.log(Drupal.settings.grievance.grievance_classification_department_map);
+
+		$('.form-item-field-grievance-class-tid-und input').once().change(function() {
+		  alert( "Handler for .change() called." );
+		});
+
+		$('.form-item-field-grievance-class-tid-und input').bind('autocompleteSelect', function() {
+			alert("Autocomplete select with " + $(this).val());
+		});
+
+
+
+
+	});
+}(jQuery));
