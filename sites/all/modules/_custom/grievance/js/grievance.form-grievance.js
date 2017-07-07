a4ec03bfd912d6e5e1a64d4b4d858d0291c58bc0
sam@clevernamehere.com <root@localhost.localdomain>
2017-07-07 13:53:14 +0000

    Fixes for ajax refresh of form

--- sites/all/modules/_custom/grievance/js/grievance.form-grievance.js
+++ sites/all/modules/_custom/grievance/js/grievance.form-grievance.js
@@ -1,57 +1,66 @@
 (function($) {
+	// console.log("Entered...");
+
 	$(document).ready(function () {
 
-		if (Drupal.jsAC) {
-		  Drupal.jsAC.prototype.select = function (node) {
-		    this.input.value = $(node).data('autocompleteValue');
-		    // Custom: add an event trigger
-		    $(this.input).trigger('autocompleteSelect', $(node).data('autocompleteValue'));
-		  };
-		}
+		$.fn.grievance_bind_classification_term_map = function() {
+			// console.log("Binding...");
 
+			if (Drupal.jsAC) {
+				// console.log("Bound.");
+			  Drupal.jsAC.prototype.select = function (node) {
+			    this.input.value = $(node).data('autocompleteValue');
+			    // Custom: add an event trigger
+			    $(this.input).trigger('autocompleteSelect', $(node).data('autocompleteValue'));
+			  };
+			}
 
-		console.log(Drupal.settings.grievance.grievance_classification_department_map);
-
-		$('.form-item-field-grievance-class-tid-und input').once().change(function() {
-			// console.log("Handler for .change() called.");
-		});
-
-		$('.form-item-field-grievance-class-tid-und input').bind('autocompleteSelect', function() {
-			// Get our map
-			map = Drupal.settings.grievance.grievance_classification_department_map;
-			if (!map) { return; }
-			// console.log(map);
-
-			// Start by enabling everything.
-			$(".form-item-field-grievance-department-tid-und option").each(function() { 
-				$(this).attr('disabled', false);
-				$(this).attr("hidden", false);
+			$('.form-item-field-grievance-class-tid-und input').once().change(function() {
+				// console.log("Handler for .change() called.");
 			});
 
-			// Get our classification name
-			class_name = $(this).val();
-			// console.log("Searching for " + class_name);
-			if (!class_name) { return; }
-			if (!map[class_name]) { return; }
+			$('.form-item-field-grievance-class-tid-und input').bind('autocompleteSelect', function() {
+				// Get our map
+				map = Drupal.settings.grievance.grievance_classification_department_map;
+				if (!map) { return; }
+				// console.log(map);
 
-			// Iterate over all the departments
-			$(".form-item-field-grievance-department-tid-und option").each(function() {
-				// Get the department TID
-				dept_tid = $(this).val();
-				if (dept_tid == '_none') { return; }
-				if (dept_tid == '') { return; }
-				// console.log("Searching for " + class_name + " / " + dept_tid + ": "); 
-				// console.log(map[class_name][dept_tid]);
+				// Start by enabling everything.
+				$(".form-item-field-grievance-department-tid-und option").each(function() { 
+					$(this).attr('disabled', false);
+					$(this).attr("hidden", false);
+				});
 
-				// If it's not in the map, disable it.
-				if (map[class_name][dept_tid]) {
-					// console.log("Found it."); 
-				} else {
-					$(this).attr("disabled", true);
-					$(this).attr("hidden", true);
-					// console.log('Not found.  Disable.');
-				}
-			});
-		});
+				// Get our classification name
+				class_name = $(this).val();
+				// console.log("Searching for " + class_name);
+				if (!class_name) { return; }
+				if (!map[class_name]) { return; }
+
+				// Iterate over all the departments
+				$(".form-item-field-grievance-department-tid-und option").each(function() {
+					// Get the department TID
+					dept_tid = $(this).val();
+					if (dept_tid == '_none') { return; }
+					if (dept_tid == '') { return; }
+					// console.log("Searching for " + class_name + " / " + dept_tid + ": "); 
+					// console.log(map[class_name][dept_tid]);
+
+					// If it's not in the map, disable it.
+					if (map[class_name][dept_tid]) {
+						// console.log("Found it."); 
+					} else {
+						$(this).attr("disabled", true);
+						$(this).attr("hidden", true);
+						// console.log('Not found.  Disable.');
+					};
+
+				}); // each
+			}); // bind
+		}; // $.fn
+
+		// console.log("Ready...");
+		$.fn.grievance_bind_classification_term_map();
+
 	});
 }(jQuery));
