f02eee0270e212a519642f5b2ca630f27cb227e9
sam@clevernamehere.com <root@localhost.localdomain>
2017-07-07 13:15:26 +0000

    Classification / department mapping

--- sites/all/modules/_custom/grievance/js/grievance.form-grievance.js
+++ sites/all/modules/_custom/grievance/js/grievance.form-grievance.js
@@ -10,21 +10,47 @@
 		}
 
 
-		// alert('Hello, world.');
-		// console.log(Drupal.settings.grievance.grievance_classification_department_map);
+		console.log(Drupal.settings.grievance.grievance_classification_department_map);
 
-		/*
 		$('.form-item-field-grievance-class-tid-und input').once().change(function() {
-		  alert( "Handler for .change() called." );
+			// console.log("Handler for .change() called.");
 		});
 
 		$('.form-item-field-grievance-class-tid-und input').bind('autocompleteSelect', function() {
-			alert("Autocomplete select with " + $(this).val());
+			// Get our map
+			map = Drupal.settings.grievance.grievance_classification_department_map;
+			if (!map) { return; }
+
+			// Start by enabling everything.
+			$(".form-item-field-grievance-department-tid-und option").each(function() { 
+				$(this).attr('disabled', false);
+				$(this).attr("hidden", false);
+			});
+
+			// Get our classification name
+			class_name = $(this).val();
+			// console.log("Searching for " + class_name);
+			if (!class_name) { return; }
+			if (!map[class_name]) { return; }
+
+			// Iterate over all the departments
+			$(".form-item-field-grievance-department-tid-und option").each(function() {
+				// Get the department TID
+				dept_tid = $(this).val();
+				if (dept_tid == '_none') { return; }
+				if (dept_tid == '') { return; }
+				// console.log("Searching for " + class_name + " / " + dept_tid + ": "); 
+				// console.log(map[class_name][dept_tid]);
+
+				// If it's not in the map, disable it.
+				if (map[class_name][dept_tid]) {
+					// console.log("Found it."); 
+				} else {
+					$(this).attr("disabled", true);
+					$(this).attr("hidden", true);
+					// console.log('Not found.  Disable.');
+				}
+			});
 		});
-		*/
-
-
-
-
 	});
 }(jQuery));
