6dfc3e51f0017fdcd39447f614d569261c290a0d
samnelson <sam@clevernamehere.com>
2020-04-13 13:50:13 +0000

    Various

--- sites/all/modules/_custom/sirius/js/sirius.esignature.js
+++ sites/all/modules/_custom/sirius/js/sirius.esignature.js
@@ -2,6 +2,50 @@
 	Drupal.behaviors.esignature = {
 	  attach: function (context) {
 
+	  	//
+	  	// Popup details
+	  	//
+
+	  	$('.sirius_esignature_popup_trigger').click(function(event) {
+	  		event.preventDefault();
+        var left = Math.floor(($(window).width() - Drupal.settings.sirius_esignature.popup_width) / 2);
+        var top = Math.floor(($(window).height() - Drupal.settings.sirius_esignature.popup_height) / 2);
+
+        var overlay = $(this).parent().find('.sirius_esignature_popup_overlay');
+        var announcement = $(this).parent().find('.sirius_esignature_popup_wrap');
+        var close = $(this).parent().find('.sirius_esignature_popup_close');
+
+        overlay.show();
+        announcement.show().css({
+          'width': Drupal.settings.sirius_esignature.popup_width,
+          'height': 'auto',
+          'position': 'fixed',
+          'top': '10%',
+          'left': '50%',
+          'transform': 'translate(-50%, 0)',
+        });
+
+        // close announcement by keyboard or mouse
+        close.click(function() {
+          overlay.fadeOut();
+          announcement.fadeOut();
+        });
+        overlay.click(function() {
+          overlay.fadeOut();
+          announcement.fadeOut();
+        });
+        $(document).keyup(function(e) {
+          if (e.keyCode == 27) {
+            overlay.fadeOut();
+            announcement.fadeOut();
+          }
+        });
+      });
+
+      //
+      // Render the interactive signature canvas
+      //
+
 	  	if ($('#sirius_esig_data').length) { 
 				$('#sirius_esig_data').parent().hide();
 				$('#sirius_esig_submit').prop('disabled', true);
@@ -30,6 +74,7 @@
 					$("#sirius_esig").addClass('sirius_esignature_pane_accepted');
 				});
 			}
+
 	  }
 	};
 })(jQuery);
