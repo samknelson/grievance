36773bfbbb2bfc1567cf821a06b970bc4dc37d1f
samnelson <sam@clevernamehere.com>
2020-11-23 18:46:03 +0000

    CKEDitor Styles

---
+++ sites/all/modules/_custom/sirius/js/ckeditor.styles.js
@@ -0,0 +1,7 @@
+if (typeof(CKEDITOR) !== 'undefined') {
+    CKEDITOR.addStylesSet('drupal',
+    [
+        { name: 'Read Aloud',   element: 'span', attributes: { 'class': 'sirius_ckstyles_script_readaloud'}},
+        { name: 'Instructions', element: 'span', attributes: { 'class': 'sirius_ckstyles_script_instructions'}},
+    ]);
+}
