d67f1f18f401c72c1eb60c40470f15239e4448f0
samnelson <sam@clevernamehere.com>
2020-12-31 14:02:02 +0000

    Map stuff

--- sites/all/modules/_custom/sirius/js/map.js
+++ sites/all/modules/_custom/sirius/js/map.js
@@ -21,8 +21,11 @@
 	  	// React when a feature is added.
 	  	var markers = new Array();
 			$(document).on('leaflet.feature', function (e, lfeature, feature) {
-				if (feature.sirius.marker.id) {
+				if (feature.sirius && feature.sirius.marker.id) {
 					markers[feature.sirius.marker.id] = lfeature;
+					if (feature.tooltip) { 
+						lfeature.bindTooltip(feature.tooltip, feature.tooltip_options);
+					}
 				}
 			});
 
@@ -31,11 +34,22 @@
 				// Don't know why this doesn't get passed in
 				map_id = map._container.id;
 
+				// Add the print control
+				/*
+				L.easyPrint({
+					title: 'Print',
+					position: 'topleft',
+					sizeModes: ['A4Portrait', 'A4Landscape']
+				}).addTo(map);
+				*/
+
 				// Handle modal links within marker popups. Normally we wouldn't bother, Drupal does this for free. 
 				// But the HTML for the popup is rendered dynamically, so Drupal doesn't get a chance to handle it. So we
 				// do some ugliness.
 				$('#' + map_id).on('click', '.modal_link', function(event) {
 					event.preventDefault();
+					if (map.isFullscreen()) { map.toggleFullscreen(); }
+					map.fire('onExitFullScreen');
 					modal_url = $(this).attr('href');
 					link = $("<a></a>").attr('href', modal_url).addClass('ctools-use-modal-processed').click(Drupal.CTools.Modal.clickAjaxLink);
 					Drupal.ajax[modal_url] = new Drupal.ajax(modal_url, link.get(0), { url: modal_url, event: 'click', progress: { type: 'throbber' } });
@@ -43,19 +57,52 @@
 				});
 
 				// Handle current location. @todo this code is ugly.
-		  	if (Drupal.settings.sirius_map.current_location.show) {
+		  	if (Drupal.settings.sirius_map.current_location && Drupal.settings.sirius_map.current_location.show) {
 					var current_location_marker;
 					map.locate({setView: false, watch: true})
         		.on('locationfound', function(e){
+        			// Mark current location
         			if (current_location_marker) { map.removeLayer(current_location_marker); }
             	current_location_marker = L.marker([e.latitude, e.longitude], {icon: icons['current_location']});
             	map.addLayer(current_location_marker);
+
+            	// Checkin
+            	sirius_map_checkin(e);
         		})
        			.on('locationerror', function(e){
        				$('#sirius_map_poll_message').html(e.message);
         		});
 		  	}
 		  });
+
+			// Send our location back to the server
+			var last_checkin_ts;
+		  function sirius_map_checkin(e) {
+		  	// Don't check in unless it's in the settings
+		  	if (!Drupal.settings.sirius_map.current_location) { return; }
+      	if (!Drupal.settings.sirius_map.current_location.checkin) { return; }
+      	if (!Drupal.settings.sirius_map.current_location.checkin.active) { return; }
+
+      	// Don't check in too often
+      	timeout = Drupal.settings.sirius_map.current_location.checkin.timeout;
+      	if (!timeout) { timeout = 1000 * 60; }
+    		if (last_checkin_ts + timeout > Date.now()) { return; } 
+    		last_checkin_ts = Date.now();
+
+    		// Checkin
+				$.ajax({
+					'url': '/sirius/ajax/map/current_location/checkin',
+					'type': 'POST',
+		  		'dataType': 'json',
+		  		'data': {'location': {'lat': e.latitude, 'lon': e.longitude} },
+		  		'error': function(jqXHR, textStatus, errorThrown) {
+		  			console.log('Current location checkin failed: ' + textStatus + ': ' + errorThrown);
+		  		},
+					'success': function(data) {
+		  			console.log('Current location complete: ' + data['msg']);
+					}
+				});
+			}
 
 			// A stupid utility function to make a random change in a marker.
 	  	function jiggle(marker) {
@@ -87,6 +134,7 @@
 					    	if (!marker) { continue; }
 					    	marker.setIcon(icons[record.icon_name]);
 					    	marker._popup.setContent(record.popup);
+    						marker.setLatLng(new L.LatLng(record.lat, record.lon));
 							}
 						}
 						date = new Date();
@@ -112,12 +160,14 @@
       	setInterval(sirius_map_poll, timeout);
       }
 
-	    Drupal.ajax.prototype.commands.sirius_command_map_update = function(ajax, response, status) {
-	    	marker = markers[response.participant_nid];
-	    	if (!marker) { return; }
-	    	marker.setIcon(icons[response.icon_name]);
-	    	marker._popup.setContent(response.popup);
-	    }
+      if (Drupal.ajax) {
+		    Drupal.ajax.prototype.commands.sirius_command_map_update = function(ajax, response, status) {
+		    	marker = markers[response.participant_nid];
+		    	if (!marker) { return; }
+		    	marker.setIcon(icons[response.icon_name]);
+		    	marker._popup.setContent(response.popup);
+		    }
+		  }
 
 	  } }
 	};
