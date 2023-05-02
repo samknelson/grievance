f2d6962a7d3ed9db0f304a4c88ec741a9febf2e8
Sam Nelson <samknelson@gmail.com>
2023-05-02 11:12:56 +0000

    Trigger change events, so other components can react

--- sites/all/modules/_custom/sirius/js/map_form.js
+++ sites/all/modules/_custom/sirius/js/map_form.js
@@ -3,13 +3,13 @@
 	  attach: function (context) { 
 
 	  	function sirius_map_form_success(position) {
-	  		$('#sirius_map_current_location_lat').val(position.coords.latitude);
-	  		$('#sirius_map_current_location_lon').val(position.coords.longitude);
-				$('#sirius_map_current_location_ts').val(position.timestamp);
+	  		$('#sirius_map_current_location_lat').val(position.coords.latitude).trigger('change');
+	  		$('#sirius_map_current_location_lon').val(position.coords.longitude).trigger('change');
+				$('#sirius_map_current_location_ts').val(position.timestamp).trigger('change');
 	  	}
 
 	  	function sirius_map_form_failure(error) {
-	  		$('#sirius_map_current_location_err').val(error.message);
+	  		$('#sirius_map_current_location_err').val(error.message).trigger('change');
 	  	}
 
 	  	navigator.geolocation.getCurrentPosition(sirius_map_form_success, sirius_map_form_failure);
